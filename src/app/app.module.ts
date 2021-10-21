1f811363e4077c49e9cd6b9d0ba2da1cb8215b40
Noel Tshuma <NTshuma2@oldmutual.com>
2021-10-21 18:38:34 +0200

    Implement Feature Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,25 @@
-import { ProductDetailGuard } from './product/product-detail.guard';
 import { WelcomeComponent } from './home/welcome.component';
-import { StarComponent } from './shared/star.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-space.pipe';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product/product-list.component';
-import { ProductDetailComponent } from './product/product-detail.component';
 import { RouterModule } from '@angular/router';
-
+import { ProductModule } from './product/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      {path: 'products', component: ProductListComponent},
-      {
-        path: 'products/:id',
-        canActivate: [ProductDetailGuard],
-        component: ProductDetailComponent
-      },
       {path: 'welcome', component: WelcomeComponent},
       {path: '', redirectTo: 'welcome', pathMatch: 'full'},
       {path: '**', redirectTo: 'Welcome', pathMatch: 'full'}
-
-    ])
+    ]),
+    ProductModule
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { ConvertToSpacesPipe } from './convert-to-space.pipe';
+import { StarComponent } from './star.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    StarComponent,
+    ConvertToSpacesPipe
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    StarComponent,
+    ConvertToSpacesPipe
+  ]
+})
+export class SharedModule { }
